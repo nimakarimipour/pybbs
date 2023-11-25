49d356ff903a96bf9f6393acf3dc49af831cb49e
Nima Karimipour <karimipour.nima@gmail.com>
2023-11-25 15:24:06 -0800

    update

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -20,7 +20,7 @@ def run_annotator():
     commands = []
     commands += ["java", "-jar", ANNOTATOR_JAR]
     commands += ['-d', OUT_DIR]
-    commands += ['-bc', 'cd {} && mvn clean compile'.format(REPO)]
+    commands += ['-bc', 'cd {} && ./annotator-command.sh'.format(REPO)]
     commands += ['-cp', '{}/paths.tsv'.format(OUT_DIR)]
     commands += ['-i', 'edu.ucr.Initializer']
     commands += ['-n', 'edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted']
---
+++ annotator-command.sh
@@ -0,0 +1,1 @@
+rm -rvf target && mvn clean compile
