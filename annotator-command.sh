99dd42f5205f9eeed9f844cd66c821bc2c254b59
nimakarimipour <karimipour.nima@gmail.com>
2024-02-05 13:05:14 -0800

    update

--- annotator-command.sh
+++ annotator-command.sh
@@ -1,2 +1,7 @@
-rm -rvf target
-mvn compile
+lib=true
+arg=true
+poly=true
+cf_version="3.42.0"
+ucrt_version="0.1-alpha-2"
+rm -rvf remote-api/target
+CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -DskipTests
