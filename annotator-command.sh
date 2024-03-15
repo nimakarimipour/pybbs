80aeda286806974eee441816aeb8a3e6cb4b7f1f
Kanak Das <kdas006@ucr.edu>
2024-03-15 22:43:17 +0000

    fix version

--- annotator-command.sh
+++ annotator-command.sh
@@ -2,6 +2,6 @@ lib=true
 arg=true
 poly=true
 cf_version="3.42.0"
-ucrt_version="0.1-alpha-2"
+ucrt_version="0.1-alpha-3-SNAPSHOT"
 rm -rvf remote-api/target
 CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -DskipTests
