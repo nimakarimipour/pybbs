e992fa2983b4829437b9082db01b8fd4ac8b8378
Kanak Das <kdas006@ucr.edu>
2024-07-30 04:05:11 +0000

    Annotations run results for pybbs

--- src/main/java/co/yiiu/pybbs/config/SiteConfig.java
+++ src/main/java/co/yiiu/pybbs/config/SiteConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.Serializable;
+import edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted;
 
 /**
  * Created by tomoya.
@@ -17,9 +18,9 @@ public class SiteConfig implements Serializable {
     private static final long serialVersionUID = -7632268193700036274L;
 
     private String datasource_driver;
-    private String datasource_url;
-    private String datasource_username;
-    private String datasource_password;
+    private @RUntainted String datasource_url;
+    private @RUntainted String datasource_username;
+    private @RUntainted String datasource_password;
 
     public String getDatasource_driver() {
         return datasource_driver;
@@ -29,27 +30,27 @@ public class SiteConfig implements Serializable {
         this.datasource_driver = datasource_driver;
     }
 
-    public String getDatasource_url() {
+    public @RUntainted String getDatasource_url() {
         return datasource_url;
     }
 
-    public void setDatasource_url(String datasource_url) {
+    public void setDatasource_url(@RUntainted String datasource_url) {
         this.datasource_url = datasource_url;
     }
 
-    public String getDatasource_username() {
+    public @RUntainted String getDatasource_username() {
         return datasource_username;
     }
 
-    public void setDatasource_username(String datasource_username) {
+    public void setDatasource_username(@RUntainted String datasource_username) {
         this.datasource_username = datasource_username;
     }
 
-    public String getDatasource_password() {
+    public @RUntainted String getDatasource_password() {
         return datasource_password;
     }
 
-    public void setDatasource_password(String datasource_password) {
+    public void setDatasource_password(@RUntainted String datasource_password) {
         this.datasource_password = datasource_password;
     }
 }
