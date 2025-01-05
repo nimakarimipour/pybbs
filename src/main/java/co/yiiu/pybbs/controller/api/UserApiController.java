f5528c378a360c6c54278ebabcc87519a5e7817c
Kanak Das <kdas006@ucr.edu>
2025-01-05 02:51:00 +0000

    Annotations run results for pybbs

--- src/main/java/co/yiiu/pybbs/controller/api/UserApiController.java
+++ src/main/java/co/yiiu/pybbs/controller/api/UserApiController.java
@@ -11,6 +11,7 @@ import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted;
 
 /**
  * Created by tomoya.
@@ -40,9 +41,9 @@ public class UserApiController extends BaseApiController {
         // 查询oauth登录的用户信息
         List<OAuthUser> oAuthUsers = oAuthUserService.selectByUserId(user.getId());
         // 查询用户的话题
-        MyPage<Map<String, Object>> topics = topicService.selectByUserId(user.getId(), 1, 10);
+        MyPage<Map<String, @RUntainted Object>> topics = topicService.selectByUserId(user.getId(), 1, 10);
         // 查询用户参与的评论
-        MyPage<Map<String, Object>> comments = commentService.selectByUserId(user.getId(), 1, 10);
+        MyPage<Map<String, @RUntainted Object>> comments = commentService.selectByUserId(user.getId(), 1, 10);
         // 查询用户收藏的话题数
         Integer collectCount = collectService.countByUserId(user.getId());
 
@@ -61,7 +62,7 @@ public class UserApiController extends BaseApiController {
         // 查询用户个人信息
         User user = userService.selectByUsername(username);
         // 查询用户的话题
-        MyPage<Map<String, Object>> topics = topicService.selectByUserId(user.getId(), pageNo, null);
+        MyPage<Map<String, @RUntainted Object>> topics = topicService.selectByUserId(user.getId(), pageNo, null);
         Map<String, Object> map = new HashMap<>();
         map.put("user", user);
         map.put("topics", topics);
@@ -74,7 +75,7 @@ public class UserApiController extends BaseApiController {
         // 查询用户个人信息
         User user = userService.selectByUsername(username);
         // 查询用户参与的评论
-        MyPage<Map<String, Object>> comments = commentService.selectByUserId(user.getId(), pageNo, null);
+        MyPage<Map<String, @RUntainted Object>> comments = commentService.selectByUserId(user.getId(), pageNo, null);
         Map<String, Object> map = new HashMap<>();
         map.put("user", user);
         map.put("comments", comments);
@@ -87,7 +88,7 @@ public class UserApiController extends BaseApiController {
         // 查询用户个人信息
         User user = userService.selectByUsername(username);
         // 查询用户参与的评论
-        MyPage<Map<String, Object>> collects = collectService.selectByUserId(user.getId(), pageNo, null);
+        MyPage<Map<String, @RUntainted Object>> collects = collectService.selectByUserId(user.getId(), pageNo, null);
         Map<String, Object> map = new HashMap<>();
         map.put("user", user);
         map.put("collects", collects);
