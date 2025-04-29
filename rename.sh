aeebfa215086b7968cab807e0c59459f5e3c112c
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-28 22:31:11 -0700

    update

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Adjusting paths"
 # Define the old and new paths
-OLD_PATH="/home/kdas006/annotation_dataset/pybbs"
+OLD_PATH="/home/nima/Desktop/taint-docker/table4/annotator/pybbs"
 NEW_PATH="$(cd "$(dirname "$0")" && pwd)"
 
 # Get absolute path of the running script
